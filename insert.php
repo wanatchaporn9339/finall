db9986ad540483ce6f2f6dd16fff87f04b7ad0f5
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-27 15:44:50 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,8 +1,25 @@
 <?php
 
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070241', 'DMLbmk22', 'it63070241_lab', 3306);
+if (mysqli_connect_errno($conn))
+{
+  die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
 
 $A = $_POST['A'];
 $B = $_POST['B'];
 $C = $A + $B;
-echo $C;
+
+$sql = "INSERT INTO exlap (A , B , C) VALUES ('$A', '$B', '$C')";
+
+if (mysqli_query($conn, $sql)) {
+  
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
 ?>
